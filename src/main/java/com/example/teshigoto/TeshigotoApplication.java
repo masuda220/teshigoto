918c96473ab9b4afce1d774162e0203a89959af1
masuda220 <masuda220@gmail.com>
2023-03-09 15:40:15 +0900

    first commit of new project : IntelliJ IDEA using Spring Initializer

---
+++ src/main/java/com/example/teshigoto/TeshigotoApplication.java
@@ -0,0 +1,13 @@
+package com.example.teshigoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeshigotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeshigotoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teshigoto/TeshigotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teshigoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeshigotoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
